92ffcebab4e31cff51dcb5e505f59dce16185e4e
David Zhang <dzhang@plaid.com>
2020-07-30 11:51:31 -0400

    more cleanup

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -16,19 +16,16 @@ const router = express.Router();
 router.post('/', asyncWrapper(async (req, res) => {
   try {
     const { userId, itemId } = req.body;
-    console.log('hm', JSON.stringify(req.body), itemId);
     let accessToken = null;
     let products = ['transactions'];
     if (itemId != null) {
       const itemIdResponse = await retrieveItemById(itemId);
-      console.log('response', JSON.stringify(itemIdResponse));
       accessToken = itemIdResponse.plaid_access_token;
       products = [];
     }
     const response = await fetch('http://ngrok:4040/api/tunnels');
     const { tunnels } = await response.json();
     const httpTunnel = tunnels.find(t => t.proto === 'http');
-    console.log('tunenls', JSON.stringify(httpTunnel));
     const linkTokenParams = {
       user: {
         client_user_id: "" + userId,
@@ -40,10 +37,9 @@ router.post('/', asyncWrapper(async (req, res) => {
       webhook: httpTunnel.public_url + '/services/webhook',
       access_token: accessToken,
     };
-    console.log('params', linkTokenParams);
     res.json(await plaid.createLinkToken(linkTokenParams));
   } catch (err) {
-    console.log('err', err);
+    console.log('error while fetching client token', err);
     throw err;
   }
 }));

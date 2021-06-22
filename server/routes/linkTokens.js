e7fd71e4e49b0315f50a3009876678acc91e4684
lindawoo-plaid <77688518+lindawoo-plaid@users.noreply.github.com>
2021-06-22 09:57:57 -0700

    fix Oauth (#80)

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -10,6 +10,17 @@ const plaid = require('../plaid');
 const fetch = require('node-fetch');
 const { retrieveItemById } = require('../db/queries');
 
+const {
+  PLAID_SANDBOX_REDIRECT_URI,
+  PLAID_DEVELOPMENT_REDIRECT_URI,
+  PLAID_ENV,
+} = process.env;
+
+const redirect_uri =
+  PLAID_ENV == 'sandbox'
+    ? PLAID_SANDBOX_REDIRECT_URI
+    : PLAID_DEVELOPMENT_REDIRECT_URI;
+
 const router = express.Router();
 
 router.post(
@@ -38,7 +49,7 @@ router.post(
         language: 'en',
         webhook: httpTunnel.public_url + '/services/webhook',
         access_token: accessToken,
-        redirect_uri: 'http://localhost:3000/oauth-link',
+        redirect_uri: redirect_uri,
       };
       const createResponse = await plaid.linkTokenCreate(linkTokenParams);
       res.json(createResponse.data);

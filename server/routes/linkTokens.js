a0c4e0d1a073c66ac06bfa0b8624cc050907dcc7
lindawoo-plaid <77688518+lindawoo-plaid@users.noreply.github.com>
2021-08-13 08:22:39 -0700

    Update linkTokens.js

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -49,7 +49,7 @@ router.post(
         access_token: accessToken,
       };
       // If user has entered a redirect uri in the .env file
-      if (redirect_uri.length > 1) {
+      if (redirect_uri.indexOf('http') === 0) {
         linkTokenParams.redirect_uri = redirect_uri;
       }
       const createResponse = await plaid.linkTokenCreate(linkTokenParams);

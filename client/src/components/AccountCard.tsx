ccab833cf7f2a9231101b6c7f7aa8d9341df91a1
Linda <lwoo@plaid.com>
2021-08-21 11:27:24 -0700

    add transferAmount to message

--- client/src/components/AccountCard.tsx
+++ client/src/components/AccountCard.tsx
@@ -23,44 +23,51 @@ export default function AccountCard(props: Props) {
       ? account.available_balance
       : account.current_balance;
   const [isAmountOkay, setIsAmountOkay] = useState(true);
-  const [showError, setShowError] = useState(false);
+  const [transferAmount, setTransferAmount] = useState(0);
+  const [
+    showTransferConfirmationError,
+    setShowTransferConfirmationError,
+  ] = useState(false);
 
   const sendRequestToProcessor = (amount: number, processorToken: string) => {
+    // placeholder code to simulate request to Dwolla/Processor:
     // api route to send processor_token and amount to transfer to
     // Dwolla or other processor initiate transfer endpoint
     console.log('sending' + amount + 'and' + processorToken + 'to processor');
-    // return some sort of confirmation from Dwolla???
+    // return confirmation from Dwolla/Processor
     return amount;
   };
 
   const completeAchTransfer = (amount: number, accountId: string) => {
+    // placeholder code to simulate ach bank transfer:
     // api route to complete ach bank transfer
     console.log(
       'completing transfer of' + amount + ' from account # ' + accountId
     );
+    // return confirmation of ach transfer
     return amount;
   };
 
   const checkAmountAndInitiate = async (amount: number) => {
     setIsAmountOkay(amount <= balance);
-    setShowError(false);
-    let confirmedAmount: number | null = 0;
+    setTransferAmount(amount);
+    setShowTransferConfirmationError(false);
+
     if (amount <= balance && amount > 0) {
-      confirmedAmount =
+      const confirmedAmount =
         IS_PROCESSOR === 'true'
           ? sendRequestToProcessor(amount, account.processor_token)
           : completeAchTransfer(amount, account.plaid_account_id);
       if (confirmedAmount == null) {
-        setShowError(true);
+        setShowTransferConfirmationError(true);
+      } else {
+        const { data: appFunds } = await updateAppFundsBalance(
+          props.userId,
+          confirmedAmount
+        );
+        props.updateAppFund(appFunds[0]);
+        props.closeView();
       }
-    }
-    if (confirmedAmount != null && confirmedAmount > 0) {
-      const { data: appFunds } = await updateAppFundsBalance(
-        props.userId,
-        confirmedAmount
-      );
-      props.updateAppFund(appFunds[0]);
-      props.closeView();
     }
   };
   return (
@@ -79,10 +86,10 @@ export default function AccountCard(props: Props) {
       {!isAmountOkay && (
         <Callout warning>
           {' '}
-          We are unable to verify that amount in your bank account.
+          We are unable to verify {transferAmount} in your bank account.
         </Callout>
       )}
-      {showError && (
+      {showTransferConfirmationError && (
         <Callout warning>
           {' '}
           Oops! Something went wrong with the transfer. Try again later.

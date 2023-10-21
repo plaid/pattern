96be4ab896eb9f3b04b697eaad5018557a48d56f
James Labastida <james@getpalm.co>
2023-10-20 19:01:38 -0700

    Update port setting to use Heroku's dynamic port or 3000 if running locally

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const {
 
 const app = express();
 
-const { PORT } = process.env;
+// Updated line for setting the port
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
@@ -55,17 +56,3 @@ app.get('/test', (req, res) => {
   res.send('test response');
 });
 
-app.use('/users', usersRouter);
-app.use('/sessions', sessionsRouter);
-app.use('/items', itemsRouter);
-app.use('/accounts', accountsRouter);
-app.use('/institutions', institutionsRouter);
-app.use('/services', serviceRouter);
-app.use('/link-event', linkEventsRouter);
-app.use('/link-token', linkTokensRouter);
-app.use('/assets', assetsRouter);
-app.use('*', unhandledRouter);
-
-// Error handling has to sit at the bottom of the stack.
-// https://github.com/expressjs/express/issues/2718
-app.use(errorHandler);
